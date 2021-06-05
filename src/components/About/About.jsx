2defae0a5785b0cd66ddefeb20d1cdda9120aa8e
Brais <braiscao.dev@gmail.com>
2021-06-05 13:53:02 +0200

    Added image to about section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,22 +1,33 @@
 import Section from 'components/Section/Section';
 import { graphql, useStaticQuery } from 'gatsby';
+import { GatsbyImage, getImage } from 'gatsby-plugin-image';
 import { MDXRenderer } from 'gatsby-plugin-mdx';
 
 export const About = () => {
   const data = useStaticQuery(
     graphql`
       query {
-        mdx(fileAbsolutePath: { regex: "/content/about.mdx/" }) {
+        mdx(fileAbsolutePath: { regex: "/content/about/about.mdx/" }) {
           body
+          frontmatter {
+            Avatar: image {
+              childImageSharp {
+                gatsbyImageData(
+                  width: 200
+                  placeholder: TRACED_SVG
+                  tracedSVGOptions: { background: "#93d9de", color: "#fccd0f" }
+                )
+              }
+            }
+          }
         }
       }
     `
   );
 
-  console.log(data);
-
   return (
     <Section title="Conóceme">
+      <GatsbyImage image={getImage(data.mdx.frontmatter.Avatar)} />
       <MDXRenderer>{data.mdx.body}</MDXRenderer>
     </Section>
   );
