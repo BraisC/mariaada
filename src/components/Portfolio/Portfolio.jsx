0a39eb589e704bdb704312922cb52d6ae3f64710
Brais <braiscao.dev@gmail.com>
2021-06-08 18:16:43 +0200

    Started portfolio items and graphql query in portfolio

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,7 +1,26 @@
 import Section from 'components/Section/Section';
+import { graphql, useStaticQuery } from 'gatsby';
 
-export const Portfolio = () => (
-  <Section title="Portfolio">
-    <p>hola</p>
-  </Section>
-);
+export const Portfolio = () => {
+  const data = useStaticQuery(
+    graphql`
+      query {
+        allMdx(filter: { fileAbsolutePath: { regex: "/content/portfolio/" } }) {
+          edges {
+            node {
+              fileAbsolutePath
+            }
+          }
+        }
+      }
+    `
+  );
+
+  console.log(data);
+
+  return (
+    <Section title="Portfolio">
+      <p>hola</p>
+    </Section>
+  );
+};
