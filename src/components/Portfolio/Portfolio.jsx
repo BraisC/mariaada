dea6d4d6043f4ddeee1d774470aa3e9ca357e23b
Brais <braiscao.dev@gmail.com>
2021-06-12 21:44:39 +0200

    Added bg image to link and order

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,12 +1,17 @@
 import Section from 'components/Section/Section';
 import { graphql, Link, useStaticQuery } from 'gatsby';
 import { GatsbyImage, getImage } from 'gatsby-plugin-image';
+import styled from 'styled-components';
+import craft from 'assets/images/bg-craft.png';
 
 export const Portfolio = () => {
   const data = useStaticQuery(
     graphql`
       query {
-        allMdx(filter: { fileAbsolutePath: { regex: "/content/portfolio/" } }) {
+        allMdx(
+          filter: { fileAbsolutePath: { regex: "/content/portfolio/" } }
+          sort: { fields: frontmatter___order, order: ASC }
+        ) {
           nodes {
             id
             # Esto sirve para generar la URL en base al slug que crea gatsby para cada pagina generada por la template
@@ -36,10 +41,14 @@ export const Portfolio = () => {
   return (
     <Section title="Portfolio">
       {data.allMdx.nodes.map((node) => (
-        <Link key={node.id} to={node.portfolioPath}>
-          <GatsbyImage image={getImage(node.frontmatter.Cover)} />
-        </Link>
+        <PortFolioItem key={node.id} to={node.portfolioPath}>
+          <GatsbyImage style={{ margin: 30 }} image={getImage(node.frontmatter.Cover)} />
+        </PortFolioItem>
       ))}
     </Section>
   );
 };
+
+const PortFolioItem = styled(Link)`
+  background-image: url(${craft});
+`;
