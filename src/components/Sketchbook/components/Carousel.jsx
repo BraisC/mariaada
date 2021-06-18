9408484635ee10f739c71ecc2c31729cd91fdf92
Brais <braiscao.dev@gmail.com>
2021-06-18 19:52:33 +0200

    Added wrapper to arrows

--- src/components/Sketchbook/components/Carousel.jsx
+++ src/components/Sketchbook/components/Carousel.jsx
@@ -4,53 +4,54 @@ import '../../../../node_modules/slick-carousel/slick/slick.css';
 import '../../../../node_modules/slick-carousel/slick/slick-theme.css';
 
 const NextArrow = ({ onClick }) => (
-  <StaticImage
-    src="../../../assets/images/arrow.png"
-    alt="Portada"
-    layout="fullWidth"
-    placeholder="tracedSVG"
-    quality={100}
-    tracedSVGOptions={{ color: '#fecb0d' }}
-    style={{
-      right: '-4rem',
-      position: 'absolute',
-      top: '50%',
-      display: 'block',
-      width: '3rem',
-      height: 'auto',
-      padding: '0',
-      transform: 'translate(0, -50%)',
-      cursor: 'pointer',
-      zIndex: '2',
-      filter: 'drop-shadow(-2px 2px 2px rgba(20, 20, 20, 0.48))',
-    }}
-    onClick={onClick}
-  />
+  <div onClick={onClick}>
+    <StaticImage
+      src="../../../assets/images/arrow.png"
+      alt="Portada"
+      layout="fullWidth"
+      quality={100}
+      tracedSVGOptions={{ color: '#fecb0d' }}
+      style={{
+        right: '-4rem',
+        position: 'absolute',
+        top: '50%',
+        display: 'block',
+        width: '3rem',
+        height: 'auto',
+        padding: '0',
+        transform: 'translate(0, -50%)',
+        cursor: 'pointer',
+        zIndex: '2',
+        filter: 'drop-shadow(-2px 2px 2px rgba(20, 20, 20, 0.48))',
+      }}
+    />
+  </div>
 );
 
 const PrevArrow = ({ onClick }) => (
-  <StaticImage
-    src="../../../assets/images/arrow.png"
-    alt="Portada"
-    layout="fullWidth"
-    placeholder="tracedSVG"
-    quality={100}
-    tracedSVGOptions={{ color: '#fecb0d' }}
-    style={{
-      left: '-3.8rem',
-      position: 'absolute',
-      top: '50%',
-      display: 'block',
-      width: '3rem',
-      height: 'auto',
-      padding: '0',
-      transform: 'translate(0, -50%) rotate(180deg)',
-      cursor: 'pointer',
-      zIndex: '2',
-      filter: 'drop-shadow(2px -2px 2px rgba(20, 20, 20, 0.48))',
-    }}
-    onClick={onClick}
-  />
+  <div onClick={onClick}>
+    <StaticImage
+      src="../../../assets/images/arrow.png"
+      alt="Portada"
+      layout="fullWidth"
+      placeholder="tracedSVG"
+      quality={100}
+      tracedSVGOptions={{ color: '#fecb0d' }}
+      style={{
+        left: '-3.8rem',
+        position: 'absolute',
+        top: '50%',
+        display: 'block',
+        width: '3rem',
+        height: 'auto',
+        padding: '0',
+        transform: 'translate(0, -50%) rotate(180deg)',
+        cursor: 'pointer',
+        zIndex: '2',
+        filter: 'drop-shadow(2px -2px 2px rgba(20, 20, 20, 0.48))',
+      }}
+    />
+  </div>
 );
 
 export const Carousel = ({ children }) => {
